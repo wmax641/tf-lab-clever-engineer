63eaf1aa20379f5b65b719d0bc5049056edb7c7b
wmax641 <github at wmax641 dot website>
2023-04-17 02:19:35 +1000

    deletion delay to 10m

--- lambda/server.py
+++ lambda/server.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 import json
 import os
 
-_SECONDS_BETWEEN_INSTANCE_TERMINATION = 300
+_SECONDS_BETWEEN_INSTANCE_TERMINATION = 600
 
 # Check the stored ssm param to see if it's been sufficient delay since last deletion
 # Returns (bool, int) if okay or not, and if not okay, the number of seconds to wait
